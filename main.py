e6c814c6e54c389000da6fa0314027884635f975
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-06-14 20:39:44 +0000

    Created function check_answer in qiuz_brain.py

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ quiz.next_question()
 while quiz.still_has_questions():
     quiz.next_question()
 
-# print("You've completed the quiz")
-# print(f"Your final score was:{quiz.score}/{quiz.question_number}")
+print("You've completed the quiz")
+print(f"Your final score was:{quiz.score}/{quiz.question_number}")
